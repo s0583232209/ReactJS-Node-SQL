fb5dbbf4aa685950ac1d918e0e3c9fa0f7674798
s-f-malot <s0583232209@gmail.com>
2026-04-26 18:45:06 +0300

    AmazonQ added some basics so its able to check with postman

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,4 +1,9 @@
-import express from'express';
-const router=express.Router()
-console.log("in users.routes")
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({ message: "Users route working" });
+});
+
 export default router;
