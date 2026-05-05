805a80973aec12592daeaf60a1567d249b142dea
s-f-malot <327582193@malot.org.il>
2026-05-05 13:56:33 +0300

    feat: signup logs in the user, (returns a token)

--- server/middleware/ExtractToken.middleware.js
+++
@@ -1,4 +0,0 @@
-export default async function MID_extractToken(req, res, next) {
-  console.log("in extract token middleware");
-  next();
-}
--- server/middleware/verifyToken.middleware.js
+++ server/middleware/verifyToken.middleware.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
-export default async function token(req, res, next) {
+export default async function verifyToken(req, res, next) {
   if (req.url.endsWith("/signup") || req.url.endsWith("/login"))return next();
-  console.log( "token middleware");
   const token = req.cookies.access_token;
   if (!token) return res.status(401).send("No token provided");
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
