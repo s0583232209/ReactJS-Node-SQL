61577d130f544e133bceed4fa167b5f80ac1f470
s-f-malot <327582193@malot.org.il>
2026-05-07 21:13:45 +0300

    after checks 2

--- server/server.js
+++ server/server.js
@@ -2,22 +2,22 @@ import express from "express";
 import cors from "cors";
 import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
-import authRouter     from "./routes/auth.routes.js";
-import userRouter     from "./routes/users.routes.js";
-import tasksRouter    from "./routes/tasks.routes.js";
-import postsRouter    from "./routes/posts.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/users.routes.js";
+import tasksRouter from "./routes/tasks.routes.js";
+import postsRouter from "./routes/posts.routes.js";
 import commentsRouter from "./routes/comments.routes.js";
-import { connect }    from "./db/connection.js";
-import verifyToken    from "./middleware/verifyToken.middleware.js";
-import log            from "./utils/logger.js";
+import { connect } from "./db/connection.js";
+import verifyToken from "./middleware/verifyToken.middleware.js";
+import log from "./utils/logger.js";
 
 configDotenv();
 
-const app  = express();
+const app = express();
 const PORT = process.env.PORT || 3001;
 const HOST = process.env.HOST || "localhost";
 
-app.use(cors({ origin: "http://localhost:5174", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -25,10 +25,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.json({ message: "Server is running" }));
 
 app.use("/api", verifyToken);
-app.use("/api/auth",             authRouter);
-app.use("/api/users",            userRouter);
-app.use("/api/:userId/tasks",    tasksRouter);
-app.use("/api/:userId/posts",    postsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/:userId/tasks", tasksRouter);
+app.use("/api/:userId/posts", postsRouter);
 app.use("/api/:userId/comments", commentsRouter);
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -41,7 +41,4 @@ app.listen(PORT, HOST, () => {
   log.info(`Server started on http://${HOST}:${PORT}`);
 });
 
-// We generate a 256-bit (32-byte) secret and convert it to a hex string
-// const secret = crypto.randomBytes(32).toString("hex");
-// console.log(secret);
 export default app;
